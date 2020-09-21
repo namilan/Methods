3cab039a19e19657827ec64a0a73aea78148e0dc
namilan <nichole.milan@gmail.com>
2020-09-21 19:56:22 -0400

    update calculate

--- src/main.java
+++ src/main.java
@@ -1,9 +1,36 @@
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean gameOver = true; 
+		boolean secondScore = true;
+		int score = 800;
+		int secondScore1 = 10000;
+		int levelCompleted = 5;
+		int secondScoreLevel = 8;
+		int bonus = 100;
+		int secondBonus = 200;
 
+		calculateScore();
+		
+		if (secondScore) {
+			int finalScoreSecond = secondScore1 + ( secondScoreLevel * secondBonus );
+			System.out.println("Your final score was " + finalScoreSecond);
+		}
+
+	}
+	
+	public static void calculateScore() {
+		boolean gameOver = true; 
+		int score = 800;
+		int levelCompleted = 5; 
+		int bonus = 100; 
+		
+		if (gameOver) {
+			int finalScore = score + ( levelCompleted * bonus); 
+			finalScore +=1000;
+			System.out.println("Your final score was " + finalScore);
+		}
 	}
 
 }
